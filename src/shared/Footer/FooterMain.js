72f3c4be6c3a97e4ae448b2fb87f6d667d8c7ee5
almahfuz <almahfuz0327@gmail.com>
2022-11-10 23:59:24 +0600

    added server link

--- src/shared/Footer/FooterMain.js
+++ src/shared/Footer/FooterMain.js
@@ -5,7 +5,7 @@ import logo from '../../assets/logo192.png'
 
 export default function FooterMain() {
     return (
-        <Footer container={true} bgDark={true} className='mt-20 bg-black rounded-none'>
+        <Footer container={true} bgDark={true} className='mt-20  rounded-none'>
             <div className="w-full">
                 <div className="grid w-full justify-between sm:flex sm:justify-between md:flex md:grid-cols-1">
                     <div className='flex mb-4'>
@@ -13,38 +13,38 @@ export default function FooterMain() {
                             src={logo}
                             alt=""
                         />
-                        <h1 className=' text-md text-white md:text-xl font-semibold'>Better Call Saul</h1>
+                        <h1 className=' text-md text-black md:text-xl font-semibold'>Better Call Saul</h1>
                     </div>
                     <div className="grid grid-cols-2 gap-8 sm:mt-4 sm:grid-cols-3 sm:gap-6">
-                        <div className=' text-white'>
-                            <Footer.Title title="Quick Menu" className='text-white' />
+                        <div className=' text-black'>
+                            <Footer.Title title="Quick Menu" className='text-black' />
                             <Footer.LinkGroup col={true}>
-                                <NavLink to='/services' className='text-white'>
+                                <NavLink to='/services' className='text-black'>
                                     Services
                                 </NavLink>
-                                <NavLink to='/blog' className='text-white'>
+                                <NavLink to='/blog' className='text-black'>
                                     Blog
                                 </NavLink>
                             </Footer.LinkGroup>
                         </div>
                         <div>
-                            <Footer.Title title="Follow us" className='text-white' />
+                            <Footer.Title title="Follow us" className='text-black' />
                             <Footer.LinkGroup col={true}>
-                                <Footer.Link href='https://www.facebook.com' className='text-white'>
+                                <Footer.Link href='https://www.facebook.com' className='text-black'>
                                     Facebook
                                 </Footer.Link>
-                                <Footer.Link href="https://www.github.com/almahfuz97" className='text-white'>
+                                <Footer.Link href="https://www.github.com/almahfuz97" className='text-black'>
                                     Github
                                 </Footer.Link>
                             </Footer.LinkGroup>
                         </div>
                         <div>
-                            <Footer.Title title="Legal" className='text-white' />
+                            <Footer.Title title="Legal" className='text-black' />
                             <Footer.LinkGroup col={true}>
-                                <Footer.Link href="#" className='text-white'>
+                                <Footer.Link href="#" className='text-black'>
                                     Privacy Policy
                                 </Footer.Link>
-                                <Footer.Link href="#" className='text-white'>
+                                <Footer.Link href="#" className='text-black'>
                                     Terms & Conditions
                                 </Footer.Link>
                             </Footer.LinkGroup>
